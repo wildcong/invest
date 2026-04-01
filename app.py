9693282b3442d618c86cc36702842c48efb884ae
wildcong <45349417+wildcong@users.noreply.github.com>
2026-04-01 15:51:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import pandas_datareader.data as web
+import FinanceDataReader as fdr
 import datetime
 import plotly.graph_objects as go
 from scipy.stats import zscore
